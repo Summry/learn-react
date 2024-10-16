40dc9a009aa59745a47b56917f62933594ed2745
Summry <nazira.fakhrurradi7@gmail.com>
2024-10-15 22:15:18 -0700

    deleted identifier files and added dynamic data output

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,10 +1,21 @@
+import myIMG from "./assets/react-core-concepts.png";
+
+const reactDescriptions = ["Fundamental", "Core", "Crucial"];
+
+function genRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
+
 function Header() {
+  const description =
+    reactDescriptions[genRandomInt(reactDescriptions.length - 1)];
+
   return (
     <header>
-      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+      <img src={myIMG} alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
-        Fundamental React concepts you will need for almost any app you are
+        {description} React concepts you will need for almost any app you are
         going to build!
       </p>
     </header>
