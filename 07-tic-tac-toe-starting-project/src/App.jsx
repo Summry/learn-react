0eea5151c648425ea7951256f328b964bcebccbc
Summry <nazira.fakhrurradi7@gmail.com>
2024-10-17 16:17:55 -0700

    initialized player component, game board, and game log components

--- 07-tic-tac-toe-starting-project/src/App.jsx
+++ 07-tic-tac-toe-starting-project/src/App.jsx
@@ -1,10 +1,18 @@
+import Player from "./components/Player/Player";
 
 function App() {
-  
-
   return (
-    <h1>React Tic-Tac-Toe</h1>
-  )
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <Player name={"PLAYER 1"} symbol={"X"} />
+          <Player name={"PLAYER 2"} symbol={"O"} />
+        </ol>
+        GAME BOARD
+      </div>
+      <div id="game-container">LOG</div>
+    </main>
+  );
 }
 
-export default App
+export default App;
