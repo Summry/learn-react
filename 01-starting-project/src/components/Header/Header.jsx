6a4c60b212e1dfe8a0403a8a3275bee351de8303
Summry <nazira.fakhrurradi7@gmail.com>
2024-10-16 19:17:25 -0700

    stored individual components and their related css files to their own component folder

--- 01-starting-project/src/components/Header/Header.jsx
+++ 01-starting-project/src/components/Header/Header.jsx
@@ -1,4 +1,5 @@
-import myIMG from "../assets/react-core-concepts.png";
+import "./Header.css";
+import myIMG from "../../assets/react-core-concepts.png";
 
 const reactDescriptions = ["Fundamental", "Core", "Crucial"];
 
