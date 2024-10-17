e314d2bb290c31a9845d2d8629b244bc987e18a2
Summry <nazira.fakhrurradi7@gmail.com>
2024-10-17 16:39:04 -0700

    refactored player component to update editing state and player name state

--- 07-tic-tac-toe-starting-project/src/components/Player/Player.jsx
+++ 07-tic-tac-toe-starting-project/src/components/Player/Player.jsx
@@ -1,13 +1,28 @@
 import React from "react";
+import { useState } from "react";
 
 export default function Player({ name, symbol }) {
+  const [isEditing, setIsEditing] = useState(false);
+
+  function handleClick() {
+    setIsEditing(!isEditing);
+  }
+
+  const playerName = isEditing ? (
+    <input type="text" value={name} required />
+  ) : (
+    <span className="player-name">{name}</span>
+  );
+
+  let buttonCaption = isEditing ? "SAVE" : "EDIT";
+
   return (
     <li>
       <span className="player">
-        <span className="player-name">{name}</span>
+        {playerName}
         <span className="player-symbol">{symbol}</span>
       </span>
-      <button>EDIT</button>
+      <button onClick={handleClick}>{buttonCaption}</button>
     </li>
   );
 }
