bdf048d1730aea7ca9e146ed3f81c18427a9846c
Summry <nazira.fakhrurradi7@gmail.com>
2024-10-23 15:53:53 -0700

    introducing tailwind css

--- inline-styling-project/src/components/AuthInputs/AuthInputs.jsx
+++ inline-styling-project/src/components/AuthInputs/AuthInputs.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import "./AuthInputs.css";
+// import "./AuthInputs.css";
 
 export default function AuthInputs() {
   const [enteredEmail, setEnteredEmail] = useState("");
