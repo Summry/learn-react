0bf34d0abffb765dd280c811d2543e65f09601a4
Summry <nazira.fakhrurradi7@gmail.com>
2024-10-22 22:28:16 -0700

    fixed user input data change - finished results table

--- practice-project-investment-calculator/src/App.jsx
+++ practice-project-investment-calculator/src/App.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import Header from "./components/Header/Header";
 import UserInput from "./components/UserInput/UserInput";
+import Results from "./components/Results/Results";
 
 const defaultInput = {
   initialInvestment: 15000,
@@ -12,11 +13,13 @@ const defaultInput = {
 export default function App() {
   const [userData, setUserData] = useState(defaultInput);
 
+  const isInputValid = userData.duration > 0;
+
   function handleUserDataChange(inputCategory, newValue) {
     setUserData((prevData) => {
       return {
         ...prevData,
-        [inputCategory]: newValue,
+        [inputCategory]: +newValue,
       };
     });
   }
@@ -25,10 +28,13 @@ export default function App() {
     <>
       <Header />
       <UserInput
-        initialUserInput={defaultInput}
+        initialUserInput={userData}
         onInputChange={handleUserDataChange}
       />
-      <section>table results</section>
+      {!isInputValid && (
+        <p className="center">Please input a proper duration!</p>
+      )}
+      {isInputValid && <Results userData={userData} />}
     </>
   );
 }
--- practice-project-investment-calculator/src/components/Results/Results.jsx
+++ practice-project-investment-calculator/src/components/Results/Results.jsx
@@ -1,5 +1,45 @@
 import React from "react";
+import "./Results.css";
+import { calculateInvestmentResults, formatter } from "../../util/investment";
 
-export default function Results() {
-  return <div>Results</div>;
+export default function Results({ userData }) {
+  const calculatedData = calculateInvestmentResults(userData);
+
+  const initialInvestment =
+    calculatedData[0].valueEndOfYear -
+    calculatedData[0].interest -
+    calculatedData[0].annualInvestment;
+
+  const formattedData = calculatedData.map((data) => {
+    const totalInterestValue =
+      data.valueEndOfYear -
+      data.annualInvestment * data.year -
+      initialInvestment;
+
+    const totalCapitalInvested = data.valueEndOfYear - totalInterestValue;
+
+    return (
+      <tr key={data.year}>
+        <td>{data.year}</td>
+        <td>{formatter.format(data.annualInvestment)}</td>
+        <td>{formatter.format(data.interest)}</td>
+        <td>{formatter.format(totalInterestValue)}</td>
+        <td>{formatter.format(totalCapitalInvested)}</td>
+      </tr>
+    );
+  });
+  return (
+    <table id="result">
+      <thead>
+        <tr>
+          <th>Year</th>
+          <th>Investment Value</th>
+          <th>Interest (Year)</th>
+          <th>Total Interest</th>
+          <th>Invested Capital</th>
+        </tr>
+      </thead>
+      <tbody>{formattedData}</tbody>
+    </table>
+  );
 }
