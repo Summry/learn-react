6a3f6bdd1f3584c140f61f1ff0389eb3f0f98307
Summry <nazira.fakhrurradi7@gmail.com>
2024-10-24 14:22:28 -0700

    input button sets the Welcome msg

--- refs-portals/src/components/Player.jsx
+++ refs-portals/src/components/Player.jsx
@@ -1,10 +1,23 @@
+import { useState } from "react";
+
 export default function Player() {
+  const [playerName, setPlayerName] = useState();
+  const [title, setTitle] = useState(false);
+
+  function handleChange(event) {
+    setPlayerName(event.target.value);
+  }
+
+  function handleClick() {
+    setTitle(true);
+  }
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {title ? playerName : "unknown entity"}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input type="text" onChange={handleChange} value={playerName} />
+        <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
   );
