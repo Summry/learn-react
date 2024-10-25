0bbce4ddec282874d4eaee0e4e27b4a7d8fbbbbd
Summry <nazira.fakhrurradi7@gmail.com>
2024-10-25 11:39:08 -0700

    removed empty string

--- refs-portals/src/components/ResultModal/ResultModal.jsx
+++ refs-portals/src/components/ResultModal/ResultModal.jsx
@@ -9,7 +9,7 @@ const ResultModal = forwardRef(function ResultModal(
     <dialog ref={ref} className="result-modal">
       <h2>You {result}</h2>
       <p>
-        The target time was <strong>{targetTime} seconds.</strong>{" "}
+        The target time was <strong>{targetTime} seconds.</strong>
       </p>
       <p>
         You stopped the timer with <strong>X seconds left.</strong>
