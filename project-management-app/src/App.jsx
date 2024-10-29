8ca8e300b4880821b33b4b6ab03d00e4008d0e54
Summry <nazira.fakhrurradi7@gmail.com>
2024-10-28 18:36:58 -0700

    initialized a couple components for project managememnt app

--- project-management-app/src/App.jsx
+++ project-management-app/src/App.jsx
@@ -1,6 +1,11 @@
+import Project from "./components/Project/Project";
+import ProjectList from "./components/ProjectList/ProjectList";
+import Home from "./components/Home/Home";
+
 function App() {
   return (
     <>
+      <Home></Home>
       <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1>
     </>
   );
